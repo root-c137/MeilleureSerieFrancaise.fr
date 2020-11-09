83a5000bd513bd4672ff00dd92809def5b37f10e
rootbu <mr.moussawalid@gmail.com>
2020-11-09 10:59:41 +0100

    design améliorer

---
+++ src/Controller/ConnexionController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConnexionController extends AbstractController
+{
+    /**
+     * @Route("/connexion", name="connexion")
+     */
+    public function index(Request $Requette)
+    {
+
+        return $this->render('connexion/index.html.twig', [
+            'controller_name' => 'ConnexionController',
+        ]);
+    }
+}
+
+
---
+++ src/Form/ConnexionType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Membre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ConnexionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Mail')
+            ->add('Mp')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Membre::class,
+        ]);
+    }
+}
